1de5a396164a570a3938f49b904916c564624af7
Eden Sung <eden.sung@ufispace.com>
2019-11-19 01:13:53 -0500

    temp

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 // Importing Modules
 const mongoose = require('mongoose');
 const express = require('express');
@@ -12,7 +12,6 @@ const routes = require('./routes');
 const app = express();
 const log = console.log;
 const PORT = process.env.PORT || 8080; // Step 1
-
 
 // Step 2
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
